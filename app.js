00a8d8a9e019aafe5ae01124b4de3eaee819fc02
Stephanie Yu <styu314@gmail.com>
2015-07-06 17:58:01 -0500

    Organized css, added table styling

--- app.js
+++ app.js
@@ -21,74 +21,81 @@ $(function() {
     }
   });
 
+  $('.puzzle-round .header').click(function(event) {
+    $(this).parent().toggleClass('closed');
+  })
+
   $('body').click(function(event) {
     $('.dropdown, .notification, .chat').removeClass('active');
   })
 
   // Courtesy of Victor Hung
   var canvas = document.getElementById('poofytoo');
-  var context = canvas.getContext('2d');
-  var elapsedTime = 0;
-  var d = Date.now()
 
-  window.addEventListener('resize', resizeCanvas, false);
+  if (canvas) {
+    var context = canvas.getContext('2d');
+    var elapsedTime = 0;
+    var d = Date.now()
 
-  function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    
-    drawBackground(); 
-  }
-  resizeCanvas();
+    window.addEventListener('resize', resizeCanvas, false);
 
-  function drawDiamond(ox, oy, w, h, c) {
-    w = w || 100;
-    h = h || 200;
-    context.beginPath();
-    context.moveTo(ox, oy);
-    context.lineTo(ox+w/2, oy+h/2);
-    context.lineTo(ox, oy+h);
-    context.lineTo(ox-w/2, oy+h/2);
-    context.closePath();
-    context.lineWidth = 0;
-    context.fillStyle = c;
-    context.fill();
-  }
+    function resizeCanvas() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      
+      drawBackground(); 
+    }
+    resizeCanvas();
 
-  function drawBackground() {
+    function drawDiamond(ox, oy, w, h, c) {
+      w = w || 100;
+      h = h || 200;
+      context.beginPath();
+      context.moveTo(ox, oy);
+      context.lineTo(ox+w/2, oy+h/2);
+      context.lineTo(ox, oy+h);
+      context.lineTo(ox-w/2, oy+h/2);
+      context.closePath();
+      context.lineWidth = 0;
+      context.fillStyle = c;
+      context.fill();
+    }
 
-    var DIA_WIDTH = 100
-    var DIA_HEIGHT = 190
-    var rA = 254, rB = 210
-    var gA = 255, gB = 220
-    var bA = 255, bB = 205
+    function drawBackground() {
 
-    w = canvas.width;
-    h = canvas.height;
-    cx = Math.ceil(w/DIA_WIDTH)+1
-    cy = Math.ceil(h/(DIA_HEIGHT/2))+1
-    var rOffset = 0, gOffset = 0, bOffset = 0;
+      var DIA_WIDTH = 100
+      var DIA_HEIGHT = 190
+      var rA = 254, rB = 210
+      var gA = 255, gB = 220
+      var bA = 255, bB = 205
 
-    for (j = 0; j < cy; ++j) {
-      for (i = 0; i < cx; ++i) {
-        rOffset = Math.round(((rB - rA) / cx) * i) + Math.floor(Math.sin(elapsedTime-i*0.8)*20)
-        gOffset = Math.round(((gB - gA) / cy) * j) 
-        bOffset = Math.round(((bB - bA) / cx) * j)
-        drawDiamond(
-          i*DIA_WIDTH+(j%2)*(DIA_WIDTH/2), 
-          j*(DIA_HEIGHT/2)-DIA_HEIGHT/2,
-          DIA_WIDTH+1,
-          DIA_HEIGHT+1,
-          'rgb(' + (rA + rOffset) + ', ' + (gA + gOffset) + ', ' + (bA + bOffset) + ')')
+      w = canvas.width;
+      h = canvas.height;
+      cx = Math.ceil(w/DIA_WIDTH)+1
+      cy = Math.ceil(h/(DIA_HEIGHT/2))+1
+      var rOffset = 0, gOffset = 0, bOffset = 0;
+
+      for (j = 0; j < cy; ++j) {
+        for (i = 0; i < cx; ++i) {
+          rOffset = Math.round(((rB - rA) / cx) * i) + Math.floor(Math.sin(elapsedTime-i*0.8)*20)
+          gOffset = Math.round(((gB - gA) / cy) * j) 
+          bOffset = Math.round(((bB - bA) / cx) * j)
+          drawDiamond(
+            i*DIA_WIDTH+(j%2)*(DIA_WIDTH/2), 
+            j*(DIA_HEIGHT/2)-DIA_HEIGHT/2,
+            DIA_WIDTH+1,
+            DIA_HEIGHT+1,
+            'rgb(' + (rA + rOffset) + ', ' + (gA + gOffset) + ', ' + (bA + bOffset) + ')')
+        }
       }
     }
-  }
 
-  function animateBackground() {
-    elapsedTime = (Date.now() - d)/1000
-    drawBackground();
-  }
+    function animateBackground() {
+      elapsedTime = (Date.now() - d)/1000
+      drawBackground();
+    }
 
-  window.setInterval(animateBackground,80);
+    window.setInterval(animateBackground,80);
+  }
 
 });
