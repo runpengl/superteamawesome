1b42c42b747e143f876de6a9ea5e46b5913217b7
Stephanie Yu <styu314@gmail.com>
2015-12-20 23:57:19 -0500

    Refactor create round stuff out

--- components/admin/editHunt.react.js
+++ components/admin/editHunt.react.js
@@ -3,20 +3,10 @@ var _ = require('lodash'),
     Q = require('q'),
     debug = require('debug')('superteamawesome:server'),
     React = require('react'),
-    update = require('react-addons-update');
+    update = require('react-addons-update'),
+    CreateRound = require('./createRound.react');
 
 module.exports = React.createClass({
-  addRoundName: function() {
-    var newState = update(this.state, {
-      newRound: {
-        names: {
-          $set: this.state.newRound.names.concat({val: '', key: this.state.newRound.names.length})
-        }
-      }
-    });
-    this.setState(newState);
-  },
-
   componentWillReceiveProps: function(newProps, oldProps) {
     this.setState(this.getInitialState(newProps));
   },
@@ -30,31 +20,8 @@ module.exports = React.createClass({
           }
         });
         this.setState(newState);
-        console.log(this.state.rounds);
       }
     }.bind(this));
-  },
-
-  createRound: function(event) {
-    var newRound = this.state.newRound;
-    if (newRound.parentRound === "None") {
-      newRound.parentRound = {
-        folderID: this.props.hunt.folderID
-      };
-    } else {
-      var roundIndex = parseInt(newRound.parentRound.split("-")[1]);
-      newRound.parentRound = this.state.rounds[roundIndex];
-    }
-
-    $.post("/admin/create/round",
-      {
-        newRound: newRound,
-        huntID: this.state.hunt.id
-      }
-    ).success(function(rounds) {
-      window.location.href = "/admin/edit";
-    }.bind(this));
-    event.preventDefault();
   },
 
   getFolderIcon: function() {
@@ -72,63 +39,8 @@ module.exports = React.createClass({
       driveFolder: _.find(props.folders, {"id": props.hunt.folderID}),
       folders: props.folders,
       hunt: props.hunt,
-      newRound: {
-        meta: '',
-        names: [{
-          val: '',
-          key: 0 // static key that doesn't rely on position in array for React rendering
-        }],
-        parentRound: "None"
-      },
       rounds: []
     };
-  },
-
-  handleParentRoundChange: function(event) {
-    this.setState(update(this.state, {
-      newRound: {
-        parentRound: {
-          $set: event.target.value
-        }
-      }
-    }));
-  },
-
-  handleRoundMetaLinkChange: function(event) {
-    var newState = update(this.state, {
-      newRound: {
-        meta: {
-          $set: event.target.value
-        }
-      }
-    });
-    this.setState(newState);
-  },
-
-  handleRoundNameChange: function(index, event) {
-    var names = this.state.newRound.names;
-    names[index].val = event.target.value;
-    var newState = update(this.state, {
-      newRound: {
-        names: {
-          $set: names
-        }
-      }
-    });
-    this.setState(newState);
-  },
-
-  removeRoundName: function(index) {
-    var names = this.state.newRound.names;
-    names.splice(index, 1);
-    var newState = update(this.state, {
-      newRound: {
-        names: {
-          $set: names
-        }
-      }
-    });
-    this.setState(newState);
   },
 
   render: function() {
@@ -177,7 +89,7 @@ module.exports = React.createClass({
                   <tbody>
                     {_this.state.rounds.map(function(round) {
                       return (
-                        <tr>
+                        <tr key={"round-row-" + round.id}>
                           <td>{round.name}</td>
                           <td>
                             <div className='folder'>
@@ -204,48 +116,10 @@ module.exports = React.createClass({
             </div>
           </li>
           <li className={(this.props.activeTab == "round" ? "active": "")}>
-            <h4>Add Rounds</h4>
-            <form onSubmit={this.createRound}>
-              <div className='form-element'>
-                <label htmlFor='parent-round'>Parent Round</label>
-                <select value={this.state.newRound.parentRound} onChange={this.handleParentRoundChange}>
-                  <option value="None">None</option>
-                  {this.state.rounds.map(function(round, index) {
-                    return (
-                      <option key={"round-" + index} value={"round-" + index}>{round.name}</option>
-                    );
-                  })}
-                </select>
-              </div>
-              <div className='form-element'>
-                <label htmlFor='meta-link'>Meta Puzzle Link</label>
-                <input defaultValue='' type='text' onChange={_this.handleRoundMetaLinkChange} />
-              </div>
-              <div className='form-element'>
-                <label htmlFor='name'>Round Name</label>
-              </div>
-              {this.state.newRound.names.map(function(name, index) {
-                var deleteButton = '';
-                if (index > 0) {
-                  deleteButton = <span className='delete' onClick={_this.removeRoundName.bind(_this, index)}>-</span>
-                }
-                return (
-                  <div className='form-element' key={"add.round." + name.key} >
-                    <input defaultValue='' type='text' onChange={_this.handleRoundNameChange.bind(_this, index)} />
-                    {deleteButton}
-                  </div>
-                );
-              })}
-              <div className='form-element'>
-                <div className='extra-text' onClick={this.addRoundName}>Add Another..</div>
-              </div>
-              <div className='form-element'>
-                <input type='submit' value={(this.state.newRound.names.length > 1) ? "Create Rounds" : "Create Round"} />
-              </div>
-            </form>
+            <CreateRound rounds={this.state.rounds} hunt={this.state.hunt} />
           </li>
           <li className={(this.props.activeTab == "puzzle" ? "active": "")}>
-            Add Puzzle
+            <h4>Add Puzzle</h4>
           </li>
           <li className={(this.props.activeTab == "settings" ? "active": "")}>
             Edit Settings
