fe38ebca971f0b1486b38b54b4fb238cf4c3ca54
Stephanie Yu <styu314@gmail.com>
2016-01-21 04:14:47 -0500

    Woops forgot the firebase ref

---
+++ web-app/firebase.js
@@ -0,0 +1,24 @@
+var Firebase = require('firebase'),
+    FirebaseTokenGenerator = require('firebase-token-generator'),
+    config = require('./config.js');
+
+// firebase setup
+var firebaseRef = new Firebase("https://" + config.firebase.host + "/");
+var tokenGenerator = new FirebaseTokenGenerator(config.firebase.secret);
+
+firebaseRef.onAuth(function(authData) {
+  if (authData) {
+    console.info("Authentication renewed successfully");
+    console.info("Auth expires at:", new Date(authData.expires * 1000));
+  } else {
+    console.info("auth expired! renewing...");
+    var newToken = tokenGenerator.createToken({ uid: config.firebase.serverId });
+    firebaseRef.authWithCustomToken(newToken, function(error, result) {
+      if (error) {
+        console.error("Firebase Authentication Failed!", error);
+      }
+    });
+  }
+});
+
+module.exports = firebaseRef;
