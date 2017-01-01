4d45ddef7d4b330cee6572edfaa1e4cf6e165a32
Vincent Siao <vincent@asana.com>
2017-01-01 00:47:03 -0800

    [extension] Render toolbar in React and update to lighter styling

--- chrome-extension/toolbar/toolbar.js
+++ chrome-extension/toolbar/toolbar.js
@@ -9,6 +9,26 @@ port.onMessage.addListener(function(event) {
 });
 
 function handleInitialData(event) {
-    console.log("found puzzle data", event.puzzle);
-    document.getElementById("puzzle_name").textContent = event.puzzle.name;
+    ReactDOM.render(
+        React.createElement(Toolbar, {puzzle: event.puzzle}),
+        document.getElementById("toolbar")
+    );
+}
+
+var r = React.DOM;
+function Toolbar(props) {
+    return r.div({ className: "Toolbar" },
+        r.span({ className: "Toolbar-puzzleStatus " + props.puzzle.status },
+            toHumanReadable(props.puzzle.status)),
+        r.span({ className: "Toolbar-puzzleName" }, props.puzzle.name),
+        r.a({
+            className: "Toolbar-link",
+            target: "_blank",
+            href: "https://docs.google.com/spreadsheets/d/" + props.puzzle.spreadsheet_id
+        }, "spreadsheet")
+    );
+}
+
+function toHumanReadable(camelCaseStr) {
+    return camelCaseStr.replace(/[A-Z]/g, " $1").toLowerCase();
 }
