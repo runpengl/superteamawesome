b55307cd70f6beecdc7627c1758589bddf1cd62b
Vincent Siao <vincent@asana.com>
2016-12-31 16:27:03 -0800

    [extension] Display toolbar on docs and slack pages

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -3,7 +3,11 @@ firebase.initializeApp(config.firebase);
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     switch (request.msg) {
         case "content_script_load":
-            if (request.location.hostname.match(/mit\.edu$/)) {
+            var hostname = request.location.hostname;
+            if (hostname.match(/mit\.edu$/) ||
+                hostname.match(/huntception\.com$/) ||
+                hostname === "superteamawesome.slack.com" ||
+                hostname === "docs.google.com") {
                 sendResponse({ msg: "init_toolbar" });
             }
             break;
