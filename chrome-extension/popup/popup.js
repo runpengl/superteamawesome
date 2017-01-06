ba81d037175dccc6792542d6805562c54b6a2c9e
Vincent Siao <vincent@asana.com>
2017-01-06 08:27:01 -0800

    [extension] Add toggle to sort by date (default) or puzzle status

--- chrome-extension/popup/popup.js
+++ chrome-extension/popup/popup.js
@@ -35,7 +35,11 @@ function initApp() {
                             key: puzzle.key
                         }))
                     });
-                    currentHuntPuzzles = puzzles;
+                    currentHuntPuzzles = puzzles.sort(function(p1, p2) {
+                        // sort by createdAt descending
+                        return new Date(p2.createdAt).getTime() -
+                            new Date(p1.createdAt).getTime();
+                    });
                     renderPopup();
                 });
         });
@@ -103,6 +107,7 @@ function renderPopup() {
             currentUser: firebase.auth().currentUser,
             currentHunt: currentHunt,
             numPuzzles: currentHuntPuzzles ? currentHuntPuzzles.length : 0,
+            puzzles: currentHuntPuzzles,
             puzzleViewersSnapshot: puzzleViewersSnapshot,
             puzzlesByStatus: puzzlesByStatus
         }),
@@ -111,69 +116,124 @@ function renderPopup() {
 }
 
 var r = React.DOM;
-function Popup(props) {
-    if (!props.currentUser) {
-        return props.isLoggingIn
-            ? r.img({ className: "Popup-loading", src: "../ripple.svg" })
-            : r.div({
-                className: "Popup-signInButton",
-                onClick: function() { startAuth(true); }
-            }, "Sign in with Google");
-    }
-    return r.div({ className: "Popup" },
-        r.div({ className: "Popup-toolbar" },
-            r.img({
-                className: "Popup-userImage",
-                src: props.currentUser.photoURL
-            }),
-            r.div({ className: "Popup-userName" }, props.currentUser.displayName),
-            r.div({
-                className: "Popup-signOutButton",
-                onClick: function() {
-                    chrome.runtime.sendMessage({ msg: "signOut" });
-                }
-            }, "Sign out")
-        ),
-        r.div({ className: "Popup-contents" },
-            props.currentHunt
-                ? r.div({ className: "Popup-currentHuntInfo" },
-                    "Current Hunt: ", r.a({
-                        href: "http://" + props.currentHunt.domain,
-                        onClick: function(event) {
-                            if (event.shiftKey || event.metaKey) {
-                                return;
+var Popup = React.createClass({
+    displayName: "Popup",
+    getInitialState: function() {
+        return { sortBy: "date" }
+    },
+    render: function() {
+        var props = this.props;
+        if (!props.currentUser) {
+            return props.isLoggingIn
+                ? r.img({ className: "Popup-loading", src: "../ripple.svg" })
+                : r.div({
+                    className: "Popup-signInButton",
+                    onClick: function() { startAuth(true); }
+                }, "Sign in with Google");
+        }
+        return r.div({ className: "Popup" },
+            r.div({ className: "Popup-toolbar" },
+                r.img({
+                    className: "Popup-userImage",
+                    src: props.currentUser.photoURL
+                }),
+                r.div({ className: "Popup-userName" }, props.currentUser.displayName),
+                r.div({
+                    className: "Popup-signOutButton",
+                    onClick: function() {
+                        chrome.runtime.sendMessage({ msg: "signOut" });
+                    }
+                }, "Sign out")
+            ),
+            r.div({ className: "Popup-contents" },
+                props.currentHunt
+                    ? r.div({ className: "Popup-currentHuntInfo" },
+                        "Current Hunt: ", r.a({
+                            href: "http://" + props.currentHunt.domain,
+                            onClick: function(event) {
+                                if (event.shiftKey || event.metaKey) {
+                                    return;
+                                }
+                                chrome.tabs.update({ url: "http://" + props.currentHunt.domain });
                             }
-                            chrome.tabs.update({ url: "http://" + props.currentHunt.domain });
-                        }
-                    }, r.strong(null, props.currentHunt.name)),
-                    props.numPuzzles
-                        ? r.div(null,
-                            "Puzzles Solved: ",
-                            r.strong(null,
-                                props.puzzlesByStatus.solved.length,
-                                "/", props.numPuzzles
+                        }, r.strong(null, props.currentHunt.name)),
+                        props.numPuzzles
+                            ? r.div(null,
+                                "Puzzles Solved: ",
+                                r.strong(null,
+                                    props.puzzlesByStatus.solved.length,
+                                    "/", props.numPuzzles
+                                )
                             )
+                            : null,
+                        r.div({
+                            className: "Popup-sortToggle",
+                            onClick: this.handleSortToggleClick
+                        },
+                            this.state.sortBy,
+                            React.createElement(SortIcon, {
+                                className: "Popup-sortIcon"
+                            })
                         )
+                    )
+                    : r.img({ className: "Popup-loading", src: "../ripple.svg" }),
+                r.div({ className: "Popup-allPuzzles" },
+                    props.currentHunt && props.puzzles
+                        ? React.createElement(AllPuzzles, {
+                            huntDomain: props.currentHunt.domain,
+                            puzzles: props.puzzles,
+                            sortBy: this.state.sortBy,
+                            puzzlesByStatus: props.puzzlesByStatus,
+                            puzzleViewersSnapshot: props.puzzleViewersSnapshot
+                        })
                         : null
                 )
-                : r.img({ className: "Popup-loading", src: "../ripple.svg" }),
-            r.div({ className: "Popup-allPuzzles" },
-                PUZZLE_STATUSES.map(function(status) {
-                    var puzzles = props.puzzlesByStatus[status];
+            )
+        );
+    },
+    handleSortToggleClick: function() {
+        switch (this.state.sortBy) {
+            case "date":
+                this.setState({ sortBy: "status" });
+                break;
+            case "status":
+                this.setState({ sortBy: "date" });
+                break;
+        }
+    }
+});
+
+var AllPuzzles = React.createClass({
+    displayName: "AllPuzzles",
+    render: function() {
+        return r.div({ className: "AllPuzzles" },
+            this.renderPuzzles()
+        );
+    },
+    renderPuzzles: function() {
+        switch (this.props.sortBy) {
+            case "date":
+                return React.createElement(PuzzleList, {
+                    huntDomain: this.props.huntDomain,
+                    puzzles: this.props.puzzles,
+                    puzzleViewersSnapshot: this.props.puzzleViewersSnapshot
+                })
+            case "status":
+                return PUZZLE_STATUSES.map(function(status) {
+                    var puzzles = this.props.puzzlesByStatus[status];
                     if (puzzles.length === 0) {
                         return;
                     }
                     return React.createElement(PuzzleList, {
                         key: status,
-                        huntDomain: props.currentHunt.domain,
+                        huntDomain: this.props.huntDomain,
                         puzzles: puzzles,
-                        puzzleViewersSnapshot: props.puzzleViewersSnapshot
+                        puzzleViewersSnapshot: this.props.puzzleViewersSnapshot
                     });
-                })
-            )
-        )
-    );
-}
+                }, this);
+        }
+    }
+});
 
 function PuzzleList(props) {
     return r.ul({ className: "PuzzleList" },
@@ -221,5 +281,17 @@ function PersonIcon() {
         r.path({
             d: "M12,14c-6.1,0-8,4-8,4v2h16v-2C20,18,18.1,14,12,14z"
         })
+    );
+}
+
+function SortIcon(props) {
+    return r.svg({
+        className: "SortIcon" + (props.className ? " " + props.className : ""),
+        viewBox: "0 -256 1792 1792" },
+        r.g({ transform: "matrix(1,0,0,-1,387.25424,1293.0169)" },
+            r.path({
+                d: "m 1024,448 q 0,-26 -19,-45 L 557,-45 q -19,-19 -45,-19 -26,0 -45,19 L 19,403 q -19,19 -19,45 0,26 19,45 19,19 45,19 h 896 q 26,0 45,-19 19,-19 19,-45 z m 0,384 q 0,-26 -19,-45 -19,-19 -45,-19 H 64 q -26,0 -45,19 -19,19 -19,45 0,26 19,45 l 448,448 q 19,19 45,19 26,0 45,-19 l 448,-448 q 19,-19 19,-45 z"
+            })
+        )
     );
 }
