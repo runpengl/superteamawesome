45377aaedea91c1d50eb49a050cc7da32ff1954e
Vincent Siao <vincent@asana.com>
2017-01-09 01:22:29 -0800

    [web-app] Add classnames to dependencies and remove old config file

--- web-app/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    GOOGLE_CLIENT_ID: "860913015488-rll6jfjfjt8456vs4bddfnh0a8t3uq5l.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: "yV1yrDo8EYmSS0zSjsYOURW8",
-    firebase: {
-        host: "sta-vincent.firebaseio.com",
-        secret: "IxRROfHqR9IfNDWp6temRkkRWgQgt94LeiUoltO2",
-        serverId: "someserveruuid"
-    }
-}
