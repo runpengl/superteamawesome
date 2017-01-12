9ec924551b0667927f9e056470d14bac69ef1090
Landon Carter <lcarter@mit.edu>
2017-01-12 17:47:11 -0500

    [dashboard] working clock

--- dashboard-2/src/App.js
+++ dashboard-2/src/App.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
-import './App.css';
+import './css/style.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
+        <div>
+          <h1 id="header">Super Team Awesome</h1>
+          <h2 id="subheader">Super Tired Asians</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <Clock></Clock>
+        <p id="puzzles">
+          puzzles go here
         </p>
+      </div>
+    );
+  }
+}
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div className="Clock" id="clock">
+        {this.state.date.toLocaleTimeString()}
       </div>
     );
   }
