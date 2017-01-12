3e9bb12057d825a9b0473270ea494941f3874a36
Landon Carter <lcarter@mit.edu>
2017-01-12 12:38:51 -0500

    [slackbot] @mention and use eventLog instead of child_changed

--- slack-bot/index.js
+++ slack-bot/index.js
@@ -3,7 +3,7 @@ var config = require('./config').config;
 var WebClient = require('@slack/client').WebClient;
 
 const postChannel = 'site-dev';
-var huntAddress = '';
+var startTime = Date.now();
 
 
 // Initialization
@@ -11,44 +11,25 @@ var slack = new WebClient(config.slack.token);
 admin.initializeApp(config.firebase);
 
 
-// Set up
-var db_copy = {};
-admin.database().ref("puzzles").once("value", function(snap) {
-    db_copy = snap.val();
-});
-admin.database()
-     .ref("hunts")
-     .orderByChild("isCurrent")
-     .equalTo(true)
-     .limitToFirst(1)
-     .on("value", function(huntSnapshots) {
-        huntSnapshots.forEach(function(huntSnapshot) {
-            huntAddress = huntSnapshot.val().domain;
-            console.log('hunt address is ' + huntAddress);
-        });
-     });
-
 // Firebase handlers initialization
-admin.database().ref("puzzles").on("child_changed", handleFirebaseValueChange);
-admin.database().ref("puzzles").on("child_added", handleFirebaseNewPuzzle);
+admin.database().ref('eventLogs').on('child_added', handleEvent);
 
+function handleEvent(snap) {
+    event = snap.val();
+    if (event.timestampMs > startTime) {
+        if (event.name === 'PuzzleCreated') {
+            admin.database().ref('puzzles').child(event.puzzleId).once("value", function(snap) {
+                postSlackMessage(postChannel, makeNewPuzzleMessage(snap.val()));
+            });
+        } else if (event.name === 'PuzzleSolutionChanged') {
+            var puzzleRef = admin.database().ref('puzzles').child(event.puzzleId);
+            var userRef = admin.database().ref('users').child(event.userId);
 
-// Firebase handlers
-function handleFirebaseValueChange(snap) {
-    // check if the puzzle is newly solved
-    if (snap.val().status === 'solved' && db_copy[snap.key].status !== 'solved') {
-        postSlackMessage(postChannel, makeSolvedMessage(snap.val()), ':pikadance:');
+            onceValue2(puzzleRef, userRef, function(puzzle, user) {
+                postSlackMessage(postChannel, makeSolvedMessage(puzzle, user), ':pikadance:');
+            });
+        }
     }
-    // update db_copy
-    db_copy[snap.key] = snap.val();
-}
-
-function handleFirebaseNewPuzzle(snap) {
-    if (huntAddress) {
-        postSlackMessage(postChannel, makeNewPuzzleMessage(snap.val()));
-    }
-    // update db
-    db_copy[snap.key] = snap.val();
 }
 
 
@@ -59,7 +40,7 @@ function postSlackMessage(channel, message, emoji) {
         channel,
         message, {
             icon_emoji: emoji,
-            username: "SuperTeamAwesomeBot",
+            username: 'SuperTeamAwesomeBot',
         },
         function(err, res) {
         if (err) {
@@ -72,22 +53,74 @@ function postSlackMessage(channel, message, emoji) {
 
 
 // Utility
-function makeSolvedMessage(puzzle) {
-    return puzzle.name + ' (' + makePuzzleAddress(puzzle.path) + ') has been solved! :correct:' +
+function makeSolvedMessage(puzzle, user) {
+    return makeSlackUserTag(user) + ' has solved ' + puzzle.name +
+           ' (' + makePuzzleAddress(puzzle) + ') :correct:' +
            (puzzle.solution ? '\n Solution: `' + puzzle.solution + '`' : '');
-
 }
 
 function makeNewPuzzleMessage(puzzle) {
     return puzzle.name + (puzzle.isMeta ? ' META' : '') + ' has been unlocked!\n' +
-           makePuzzleAddress(puzzle.path) + '\n' +
+           makePuzzleAddress(puzzle) + '\n' +
            'Join the slack channel ' + makeSlackChannelLink(puzzle);
 }
 
-function makePuzzleAddress(puzzlePath) {
-    return 'http://' + huntAddress + puzzlePath;
+function makePuzzleAddress(puzzle) {
+    return 'http://' + puzzle.host + puzzle.path;
 }
 
 function makeSlackChannelLink(puzzle) {
     return '<#' + puzzle.slackChannelId + '|' + puzzle.slackChannel + '>';
+}
+
+function makeSlackUserTag(user) {
+    if (user.slackUserId) {
+        return '<@' + user.slackUserId + '>';
+    }
+    else {
+        return user.displayName;
+    }
+}
+
+
+// Stolen from Vincent
+/**
+ * Takes two firebase refs and attaches `value` event listeners on them, calling
+ * the given callback when both are resolved and for every update after. The caller
+ * is responsible for calling the returned detach function.
+ */
+function onValue2(ref1, ref2, callback) {
+    var snap1 = null;
+    var snap2 = null;
+    function update() {
+        if (snap1 && snap1.val() &&
+            snap2 && snap2.val()) {
+            callback(snap1, snap2);
+        }
+    }
+    function on1(snap) { snap1 = snap; update(); }
+    function on2(snap) { snap2 = snap; update(); }
+    ref1.on("value", on1);
+    ref2.on("value", on2);
+
+    return function detach() {
+        ref1.off("value", on1);
+        ref2.off("value", on2);
+    };
+}
+
+// The same thing as above, but only once, and passes the val()'s
+function onceValue2(ref1, ref2, callback) {
+    var snap1 = null;
+    var snap2 = null;
+    function update() {
+        if (snap1 && snap1.val() &&
+            snap2 && snap2.val()) {
+            callback(snap1.val(), snap2.val());
+        }
+    }
+    function on1(snap) { snap1 = snap; update(); }
+    function on2(snap) { snap2 = snap; update(); }
+    ref1.once("value", on1);
+    ref2.once("value", on2);
 }
