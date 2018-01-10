9bcebb9e75c7405aeb58e1ff942c0183c9328641
Vincent Siao <vincent@asana.com>
2018-01-10 04:38:05 -0800

    [chrome-ext] Use CommonChunksPlugin to bundle react+react-dom

--- chrome-extension/webpack.config.js
+++ chrome-extension/webpack.config.js
@@ -1,11 +1,13 @@
 const path = require("path");
+const webpack = require("webpack");
 
 const config = {
     entry: {
         background: "./src/background/background.js",
         popup: "./src/popup/popup.js",
         sidebar: "./src/sidebar/sidebar.js",
-        toolbar: "./src/toolbar/toolbar.js"
+        toolbar: "./src/toolbar/toolbar.js",
+        common: ["classnames", "react", "react-dom"]
     },
     output: {
         path: path.resolve(__dirname, "dist"),
@@ -15,7 +17,12 @@ const config = {
         rules: [
             { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
         ]
-    }
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "common"
+        })
+    ]
 };
 
 module.exports = config;
