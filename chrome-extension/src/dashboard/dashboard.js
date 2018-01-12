242311e3648ac1344dd06116a3095294e1180e1f
Landon Carter <lcarter@palantir.com>
2018-01-11 21:13:14 -0500

    dashboard working, clickable dots

--- chrome-extension/src/dashboard/dashboard.js
+++ chrome-extension/src/dashboard/dashboard.js
@@ -19,26 +19,26 @@ function initApp() {
                     currentUser: event.user,
                     permissionDenied: false
                 });
-                return renderDashboard();
+                return renderDashboard(dashboardData);
 
             case "slackConnectionStatus":
                 dashboardData = Object.assign({}, dashboardData, {
                     slackConnectionStatus: event.status
                 });
-                return renderDashboard();
+                return renderDashboard(dashboardData);
 
             case "permissionDenied":
                 dashboardData = {
                     currentUser: dashboardData.currentUser,
                     permissionDenied: true
                 };
-                return renderDashboard();
+                return renderDashboard(dashboardData);
 
             case "hunt":
                 dashboardData = Object.assign({}, dashboardData, {
                     currentHunt: event.hunt
                 });
-                return renderDashboard();
+                return renderDashboard(dashboardData);
 
             case "puzzles":
                 var puzzlesByStatus = {
@@ -80,125 +80,75 @@ function initApp() {
                     puzzlesByStatus: puzzlesByStatus,
                     puzzleGroups: puzzleGroups
                 });
-                return renderDashboard();
+                return renderDashboard(dashboardData);
 
             case "puzzleViewers":
                 dashboardData = Object.assign({}, dashboardData, {
                     puzzleViewers: event.puzzleViewers
                 });
-                return renderDashboard();
+                return renderDashboard(dashboardData);
         }
     });
 }
 
-// window.onload = function() {
-//     ReactDOM.render(
-//         React.createElement(Clock),
-//         document.getElementById("clock")
-//     );
-//     ReactDOM.render(
-//         React.createElement(StaPrompt, {prompts: prompts}),
-//         document.getElementById("subheader")
-//     );
-// };
-
-// var port = chrome.runtime.connect({ name: "dashboardLoad" });
-// port.onMessage.addListener(function(event) {
-//     console.log(event);
-//     switch (event.msg) {
-//         case "puzzles":
-//             // Group by meta puzzles; ungrouped puzzles at the end
-//             var puzzleGroups = event.puzzles
-//                 .filter(function(p) { return p.isMeta; })
-//                 .map(function(mp) { return [mp]; });
-//             var otherPuzzles = [];
-//             var metaIndexByKey = {};
-//             puzzleGroups.forEach(function(pg, i) {
-//                 metaIndexByKey[pg[0].key] = i;
-//             });
-//             event.puzzles.forEach(function(p) {
-//                 if (p.parent) {
-//                     if (metaIndexByKey.hasOwnProperty(p.parent)) {
-//                         puzzleGroups[metaIndexByKey[p.parent]].push(p);
-//                         return;
-//                     }
-//                 }
-//                 if (!p.isMeta) {
-//                     otherPuzzles.push(p);
-//                 }
-//             });
-//             if (otherPuzzles.length) {
-//                 puzzleGroups.push(otherPuzzles);
-//             }
-//             ReactDOM.render(
-//                 React.createElement(DashboardRounds, { puzzleGroups: puzzleGroups }),
-//                 document.getElementById("puzzles")
-//             );
-//             break;
-//     }
-// });
-
-// var r = React.DOM;
-// function DashboardRounds(props) {
-//     if (props.puzzleGroups.length == 0) {
-//         return r.div({className: "Dashboard-nopuzzles"}, "No puzzles unlocked yet!");
-//     }
-//     return r.div(null,
-//         props.puzzleGroups.map(function(pg) {
-//             if (pg[0].isMeta) {
-//                 return r.div({ className: "Dashboard-round" },
-//                     r.span({
-//                         className: "Dashboard-puzzle " + pg[0].status
-//                     }, pg[0].name),
-//                     r.span({ className: "Dashboard-roundName" }, pg[0].name),
-//                     r.div({ className: "Dashboard-puzzles" },
-//                         pg.slice(1).map(function(puzzle) {
-//                             return r.span({
-//                                 key: puzzle.key,
-//                                 className: "Dashboard-puzzle " + puzzle.status
-//                             }, puzzle.name)
-//                         })
-//                     )
-//                 )
-//             } else {
-//                 return r.div({ className: "Dashboard-otherPuzzles" },
-//                     r.div({ className: "Dashboard-otherPuzzlesHeader" }, "Other Puzzles"),
-//                     pg.map(function(puzzle) {
-//                         return r.span({
-//                             key: puzzle.key,
-//                             className: "Dashboard-puzzle " + puzzle.status
-//                         }, puzzle.name);
-//                     })
-//                 );
-//             }
-//         })
-//     );
-// }
+var port = chrome.runtime.connect({ name: "dashboardLoad" });
+port.onMessage.addListener(function(event) {
+    console.log(event);
+    switch (event.msg) {
+        case "puzzles":
+            // Group by meta puzzles; ungrouped puzzles at the end
+            var puzzleGroups = event.puzzles
+                .filter(function(p) { return p.isMeta; })
+                .map(function(mp) { return [mp]; });
+            var otherPuzzles = [];
+            var metaIndexByKey = {};
+            puzzleGroups.forEach(function(pg, i) {
+                metaIndexByKey[pg[0].key] = i;
+            });
+            event.puzzles.forEach(function(p) {
+                if (p.parent) {
+                    if (metaIndexByKey.hasOwnProperty(p.parent)) {
+                        puzzleGroups[metaIndexByKey[p.parent]].push(p);
+                        return;
+                    }
+                }
+                if (!p.isMeta) {
+                    otherPuzzles.push(p);
+                }
+            });
+            if (otherPuzzles.length) {
+                puzzleGroups.push(otherPuzzles);
+            }
+            ReactDOM.render(
+                React.createElement(DashboardRounds, { puzzleGroups: puzzleGroups, huntDomain: event.huntDomain }),
+                document.getElementById("puzzles")
+            );
+            break;
+    }
+});
 
 //
 // Rendering
 // ----------------------------------------------------------------------------
 
 var dashboardData = null;
-// function renderDashboard() {
-//     ReactDOM.render(
-//         React.createElement(Dashboard, dashboardData),
-//         document.getElementById("dashboard")
-//     );
-// }
 
-console.log(promptsList)
-
-function renderDashboard() {
+function renderDashboard(dashboardData) {
+    // console.log("dashboard data");
+    // console.log(dashboardData)
     ReactDOM.render(
         React.createElement(Clock, null),
         document.getElementById("clock")
     );
-    console.log(promptsList);
+    // console.log(promptsList);
     ReactDOM.render(
         React.createElement(BackronymPrompt, {prompts: promptsList}),
         document.getElementById("subheader")
     );
+    // ReactDOM.render(
+    //     React.createElement(DashboardRounds, { puzzleGroups: [], huntDomain: dashboardData.currentHunt.domain }),
+    //     document.getElementById("puzzles")
+    // );
 }
 
 var r = {};
@@ -274,5 +224,54 @@ class BackronymPrompt extends React.Component {
     _updatePrompt() {
         var newPrompt = this.props.prompts.promptsList[Math.floor(Math.random() * this.props.prompts.promptsList.length)];
         this.setState({ prompt: newPrompt });
+    }
+}
+
+class DashboardRounds extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        console.log(this.props);
+        if (this.props.puzzleGroups.length == 0) {
+            return (
+                <div className="Dashboard-nopuzzles">
+                    No puzzles unlocked yet!
+                </div>
+            );
+        } else {
+            return r.div(null,
+                this.props.puzzleGroups.map(function(pg) {
+                    if (pg[0].isMeta) {
+                        return r.div({ className: "Dashboard-round" },
+                            r.a({
+                                className: "Dashboard-puzzle " + pg[0].status,
+                                href: "http://" + pg[0].host + pg[0].path
+                            }),
+                            r.span({ className: "Dashboard-roundName" }, pg[0].name),
+                            r.div({ className: "Dashboard-puzzles" },
+                                pg.slice(1).map(function(puzzle) {
+                                    return r.a({
+                                        key: puzzle.key,
+                                        className: "Dashboard-puzzle " + puzzle.status,
+                                        href: "http://" + puzzle.host + puzzle.path})
+                                })
+                            )
+                        )
+                    } else {
+                        return r.div({ className: "Dashboard-otherPuzzles" },
+                            r.div({ className: "Dashboard-otherPuzzlesHeader" }, "Other Puzzles"),
+                            pg.map(function(puzzle) {
+                                return r.span({
+                                    key: puzzle.key,
+                                    className: "Dashboard-puzzle " + puzzle.status
+                                }, puzzle.name);
+                            })
+                        );
+                    }
+                })
+            );
+        }
     }
 }
