bcb38dd0f4bbdc65b6f0796a26cbb6b6bf458bc0
poofytoo <victorhung92@gmail.com>
2018-01-12 10:58:06 -0500

    hunt starting website

--- web-app/server/routes/index.ts
+++ web-app/server/routes/index.ts
@@ -2,6 +2,10 @@ import * as express from "express";
 
 export const router = express.Router();
 
+router.get("/start", (_req: express.Request, res: express.Response) => {
+  res.render("start", {});
+});
+
 router.get("*", (_req: express.Request, res: express.Response) => {
   res.render("index", {});
 });
